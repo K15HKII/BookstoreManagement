5b4371a3076b540ae0e42e57e38fd54b04f43143
CrazyDopeFox <nghikhoi1108@gmail.com>
2022-06-05 13:12:55 +0700

    .

--- BookstoreManagement/Components/Converters/TextFormatConverter.cs
+++ BookstoreManagement/Components/Converters/TextFormatConverter.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Data;
 using MaterialDesignThemes.Wpf;
 
-namespace BookstoreManagement.Components.Converters
+namespace BookstoreManagement.Components
 {
     public class TextFormatConverter : IValueConverter
     {
--- BookstoreManagement/Components/Panels/VoucherPanel.xaml.cs
+++ BookstoreManagement/Components/Panels/VoucherPanel.xaml.cs
@@ -1,20 +1,9 @@
-﻿using BookstoreManagement.Data.Model.Api;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using BookstoreManagement.Data.Model.Api;
 
-namespace BookstoreManagement.Components.Panels
+namespace BookstoreManagement.Components
 {
     /// <summary>
     /// Interaction logic for VoucherPanel.xaml
