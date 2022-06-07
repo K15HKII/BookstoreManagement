c3d079cf2927d77e622824285e447c6c33e0ed66
Trí Trần <20520327@gm.uit.edu.vn>
2022-06-07 08:17:41 +0700

    .

---
+++ BookstoreManagement/ViewModels/Manager/EmployeeAdapter/EmployeeInfoNavigator.cs
@@ -0,0 +1,17 @@
+﻿using BookstoreManagement.ViewModels.DialogView.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreManagement.ViewModels.Manager.EmployeeAdapter
+{
+    public class EmployeeInfoNavigator : IEmployeeInfoNavigator
+    {
+        public object? OpenEditEmployeeDialog(EditEmployeeViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
